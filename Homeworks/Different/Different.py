39c5d1a263bf70690eb9fc55f72c3c5cbee95763
Avonakub <avonakub@icloud.com>
2026-03-01 20:58:48 +0300

    add 4 task and update 3 task and def for check number

--- Homeworks/Different/Different.py
+++ Homeworks/Different/Different.py
@@ -9,28 +9,33 @@ def show_menu():
     print("➤➤➤")
 
 
-def prime_number(text):
+def get_positive_integer(text, min_value=1):
     while True:
         user_input = input(text)
-        if user_input == "0":
-            print("Returning to Menu")
-            return
         try:
             number = int(user_input)
-            if number <= 1:
-                print("Please enter a number greater than 1.")
-                continue
-
-            div = 2
-            while number % div != 0:
-                div += 1
-            if div == number:
-                print(f"{number}  a prime number.")
+            if number >= min_value:
+                return number
             else:
-                print(f"{number} is not a prime number.")
-
+                print(f"Please enter a number greater than {min_value - 1}.")
         except ValueError:
             print("Error: Please enter only numbers!")
+
+
+def prime_number(number):
+    div = 2
+    while number % div != 0:
+        div += 1
+    if div == number:
+        print(f"{number} a prime number.")
+    else:
+        print(f"{number} is not a prime number.")
+
+
+def greatest_common_divisor(a, b):
+    while b:
+        a, b = b, a % b
+    return a
 
 
 is_continue = True
@@ -39,7 +44,14 @@ while is_continue:
     user_choice = input("Enter your choice HERE ➤ : ")
 
     if user_choice == "3":
-        num = prime_number("Enter a positive integer (or '0' to back to menu): ")
+        number = get_positive_integer("Enter a number greater than 1 to check if it's prime: ", 2)
+        prime_number(number)
+
+    elif user_choice == "4":
+        num1 = get_positive_integer("Enter first number: ")
+        num2 = get_positive_integer("Enter second number: ")
+        result = greatest_common_divisor(num1,num2)
+        print(f"The greatest common divisor of {num1} and {num2} is {result}")
 
     elif user_choice == "0":
         print("Goodbye!")
