f7ce2e73a945116d919522513a01b07a732279e4
Avonakub <avonakub@icloud.com>
2026-03-02 00:11:21 +0300

    add 5 task

--- Homeworks/Different/Different.py
+++ Homeworks/Different/Different.py
@@ -9,15 +9,15 @@ def show_menu():
     print("➤➤➤")
 
 
-def get_positive_integer(text, min_value=1):
+def get_validated_integer(text, min_value=None):
     while True:
         user_input = input(text)
         try:
             number = int(user_input)
-            if number >= min_value:
-                return number
-            else:
-                print(f"Please enter a number greater than {min_value - 1}.")
+            if min_value is not None and number < min_value:
+                print(f"Please enter a number greater than or equal to {min_value}.")
+                continue
+            return number
         except ValueError:
             print("Error: Please enter only numbers!")
 
@@ -38,20 +38,54 @@ def greatest_common_divisor(a, b):
     return a
 
 
+def caesar_cipher(text, step):
+    result = ""
+
+    for letters in text:
+        if "a" <= letters.lower() <= "z":
+            start = ord("A") if letters.isupper() else ord("a")  # Если А, то один код, если а, то начальный другой
+
+            new_pos = (ord(letters) - start + step) % 26
+            result += chr(start + new_pos)
+        else:
+            result += letters  # Пробелы и знаки как есть
+    return result
+
+
 is_continue = True
 while is_continue:
     show_menu()  # Вызвали функцию
     user_choice = input("Enter your choice HERE ➤ : ")
 
     if user_choice == "3":
-        number = get_positive_integer("Enter a number greater than 1 to check if it's prime: ", 2)
+        number = get_validated_integer("Enter a number greater than 1 to check if it's prime: ", 2)
         prime_number(number)
 
     elif user_choice == "4":
-        num1 = get_positive_integer("Enter first number: ")
-        num2 = get_positive_integer("Enter second number: ")
+        num1 = get_validated_integer("Enter first number: ")
+        num2 = get_validated_integer("Enter second number: ")
         result = greatest_common_divisor(num1,num2)
         print(f"The greatest common divisor of {num1} and {num2} is {result}")
+
+    elif user_choice == "5":
+        while True:
+            text_to_encrypt = input("Enter an English string for encryption or decryption: ")
+
+            has_russian = False
+            for letters in text_to_encrypt:
+                if 'а' <= letters.lower() <= 'я' or letters.lower() == 'ё':
+                    has_russian = True
+                    break
+            if has_russian:
+                print("Error: Russian letters are not supported!")
+            else:
+                break
+
+        print("- for encryption select a positive step\n"
+              "- for decryption - a negative step")
+        user_step = get_validated_integer("Enter the shift step: ")
+        encrypted_text = caesar_cipher(text_to_encrypt, user_step)
+        print(f"New string is: {encrypted_text}")
 
     elif user_choice == "0":
         print("Goodbye!")
