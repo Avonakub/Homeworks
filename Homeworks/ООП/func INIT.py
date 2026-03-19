702db76c3dddcf920d946ccdbe818f5ee00c530f
Avonakub <avonakub@icloud.com>
2026-03-20 02:18:54 +0300

    add 2 task

--- Homeworks/ООП/func INIT.py
+++ Homeworks/ООП/func INIT.py
@@ -14,28 +14,34 @@ class Robot:
         self.y = y
         self.history = []
 
+
     def left(self):
         self.x -= 1
         print("Robot moved left ⬅️")
         self.history.append(self.x)
+
 
     def right(self):
         self.x += 1
         print("Robot moved right ➡️")
         self.history.append(self.x)
 
+
     def up(self):
         self.y += 1
         print("Robot moved up ⬆️")
         self.history.append(self.y)
+
 
     def down(self):
         self.y -= 1
         print("Robot moved down ⬇️")
         self.history.append(self.y)
 
+
     def position(self):
         print(f"Current robot position: x = {self.x}, y = {self.y}")
+
 
     def limiter(self):
         limiter_left, limiter_right, limiter_up, limiter_down = -9, 10, 8, -15
@@ -63,11 +69,6 @@ class Robot:
             self.history.append((self.x, self.y))
 
 
-my_robot = Robot(0, 0)
-print("🟢 Robot created and launched!")
-my_robot.position()
-
-
 def robot_menu():
     print('=' * 50)
     print("CHOOSE ROBOT ACTION: \n"
@@ -80,12 +81,90 @@ def robot_menu():
           "0 - Exit")
 
 
+my_robot = Robot(0, 0)
+# ====================== Class Math ==============================
+class Math:
+    def __init__(self):
+        pass
+
+
+    def get_numbers(self):
+
+        while True:
+            try:
+                first_number = input("Enter first number: ")
+                if first_number.startswith('0') and len(first_number) > 1 and first_number[1] != '.'\
+                        or first_number.startswith('-0') and len(first_number) > 2 and first_number[2] != '.':
+                    print("Error: Value cannot start with zero (except 0.86!)")
+                    continue
+                first_number = float(first_number)
+                break
+            except ValueError:
+                print("❌ Error: Please enter a valid number.")
+
+        while True:
+            try:
+                second_number = input("Enter second number: ")
+                if second_number.startswith('0') and len(second_number) > 1 and second_number[1] != '.' \
+                        or second_number.startswith('-0') and len(second_number) > 2 and second_number[2] != '.':
+                    print("Error: Value cannot start with zero (except 0.86!)")
+                second_number = float(second_number)
+                return first_number, second_number
+            except ValueError:
+                print("❌ Error: Please enter a valid number.")
+
+
+    def addition(self, first_number, second_number):
+        result = first_number + second_number
+        print(f"{first_number} + {second_number} = {result}")
+        return result
+
+
+    def subtraction(self, first_number, second_number):
+        result = first_number - second_number
+        print(f"{first_number} - {second_number} = {result}")
+        return result
+
+
+    def multiplication(self, first_number, second_number):
+        result = first_number * second_number
+        print(f"{first_number} * {second_number} = {result}")
+        return result
+
+
+    def division(self, first_number, second_number):
+        if second_number == 0:
+            print("❌ Error: Division by zero!")
+            return None
+        else:
+            result = first_number / second_number
+            print(f"{first_number} / {second_number} = {result:.2f}")
+            return result
+
+
+def math_menu():
+    print('=' * 50)
+    print("CHOOSE MATH OPERATION: \n"
+          "1 - Addition\n"
+          "2 - Subtraction\n"
+          "3 - Multiplication\n"
+          "4 - Division\n"
+          "0 - Exit")
+
+
+my_math = Math()
+
+
 is_continue = True
 while is_continue:
     show_menu()  # Вызвали функцию
     user_choice = input("Enter your choice HERE ➤ : ")
 
     if user_choice == "1":
+
+        my_robot = Robot(0, 0)
+        print("🟢 Robot created and launched!")
+        my_robot.position()
 
         while True:
             robot_menu()  # Меню для отображения вариантов хода робота
@@ -111,6 +190,34 @@ while is_continue:
                 break
             else:
                 print("❌ Invalid choice! Try again.")
+
+    elif user_choice == "2":
+        while True:
+            math_menu()
+            user_choice = input("Enter your choice HERE ➤ : ")
+            if user_choice == '1':
+                first, second = my_math.get_numbers()
+                my_math.addition(first, second)
+            elif user_choice == '2':
+                first, second = my_math.get_numbers()
+                my_math.subtraction(first, second)
+            elif user_choice == '3':
+                first, second = my_math.get_numbers()
+                my_math.multiplication(first, second)
+            elif user_choice == '4':
+                first, second = my_math.get_numbers()
+                my_math.division(first, second)
+            elif user_choice == "0":
+                print("↩️Returning to main menu...")
+                break
+            else:
+                print("❌ Invalid choice! Try again.")
+
+
+
+
+
+
 
     elif user_choice == "0":
         print("👋 Goodbye!")
