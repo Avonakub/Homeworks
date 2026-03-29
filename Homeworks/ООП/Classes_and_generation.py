e7f95b62e0edf43855dd97889d57a4956acf1fc6
Avonakub <avonakub@icloud.com>
2026-03-29 11:46:36 +0300

    добавлена валидация значений

--- Homeworks/ООП/Classes_and_generation.py
+++ Homeworks/ООП/Classes_and_generation.py
@@ -1,11 +1,55 @@
 class Goods:
-    def __init__(self, product_name, name_store, price ):
+    def __init__(self, product_name, name_store, price):
         self.__product_name = product_name
         self.__name_store = name_store
         self.__price = price
 
 
+    @property
+    def product_name(self):
+        return self.__product_name
+
+
+    @property
+    def name_store(self):
+        return self.__name_store
+
+
+    @property
+    def price(self):
+        return f"Цена товара: {self.__price}"
+
+    @product_name.setter
+    def product_name(self, product_name):
+        if product_name is None:
+            raise TypeError("Название продукта не может быть пустым")
+        self.__product_name = product_name
+
+
+    @name_store.setter
+    def name_store(self, name_store):
+        if name_store is None:
+            raise TypeError("Название магазина не может быть пустым")
+        self.__name_store = name_store
+
+
+    @price.setter
+    def price(self, new_price):
+        if not isinstance(new_price, (int, float)):
+            raise TypeError("Цена должна быть числом!")
+        if new_price <= 0:
+            raise ValueError("Цена не может быть отрицательной или отсутствовать")
+        self.__price = new_price
+
+
+
+
 class Warehouse:
+    def __init__(self, product_name):
+        self.__product_name = []
+        pass
+
+
 
 
 
