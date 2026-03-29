c3efe70cbde972891e78cecd3ed6454625782add
Avonakub <avonakub@icloud.com>
2026-03-29 11:12:03 +0300

    добавлено меню и класс товаров

---
+++ Homeworks/ООП/Classes_and_generation.py
@@ -0,0 +1,22 @@
+class Goods:
+    def __init__(self, product_name, name_store, price ):
+        self.__product_name = product_name
+        self.__name_store = name_store
+        self.__price = price
+
+
+class Warehouse:
+
+
+
+
+
+def show_menu():
+    print("1 - Товары на складе\n"
+          "2 - ПчёлоСлон\n"
+          "3 - Посадка/высадка в автобусе\n"
+          "4 - Генератор. Спецификатор\n"
+          "5 - Генератор. Пачки повторов"
+          "0 - Выход")
+    print("➤➤➤")
+
